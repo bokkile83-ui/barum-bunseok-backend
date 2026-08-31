c0aa8f6bbf7f171ea7e913f4f6339be2a44e4056
bokkile83-ui <bokkile83@gmail.com>
2026-08-31 19:29:06 +0900

    Add files via upload

--- coverage_benchmark.py
+++ coverage_benchmark.py
@@ -1,4 +1,4 @@
-# ===== BARUM coverage_benchmark.py v619-gap-20260830 (구 v33-ci-rate-20260708 계승) =====
+# ===== BARUM coverage_benchmark.py v621-robot2v-20260831 (구 v33-ci-rate-20260708 계승) =====
 # -*- coding: utf-8 -*-
 """
 BARUM 충족률 엔진 + map_excel_to_report
